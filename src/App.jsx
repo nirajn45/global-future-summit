ef1ea52022f40505b08d411072297071212cec38
Suryavanshi11444 <sagarmodi2005@gmail.com>
2025-02-23 16:56:01 +0530

    save all and add all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
 import HeroSection from "./components/heroSection";
+import Gallery from "./components/Gallery";
+
 function App() {
   return (
-    <div className="bg-black min-h-screen flex items-center justify-center">
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center">
       <HeroSection />
+      <Gallery />
     </div>
   );
 }
