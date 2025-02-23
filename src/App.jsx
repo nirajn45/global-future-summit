ed89400634a57496157fd79f85242d7474fb0ea3
Your Name <your.email@example.com>
2025-02-23 16:56:08 +0530

    save all changes and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import HeroSection from "./components/heroSection";
+import CompanyLogos from "./components/sumit";
 function App() {
   return (
-    <div className="bg-black min-h-screen flex items-center justify-center">
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center">
       <HeroSection />
+      <CompanyLogos/>
     </div>
+    
   );
 }
 
