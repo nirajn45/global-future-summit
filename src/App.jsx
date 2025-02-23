17c3e40e01d1cae7e0f18fba7e02672808f72f95
Suryavanshi11444 <sagarmodi2005@gmail.com>
2025-02-23 21:23:08 +0530

    save all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import HeroSection from "./components/heroSection";
-<<<<<<< HEAD
+
 import Gallery from "./components/Gallery";
 
-=======
+
 import CompanyLogos from "./components/sumit";
->>>>>>> ed89400634a57496157fd79f85242d7474fb0ea3
+
 function App() {
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center">
       <HeroSection />
-<<<<<<< HEAD
-      <Gallery />
-=======
       <CompanyLogos/>
->>>>>>> ed89400634a57496157fd79f85242d7474fb0ea3
+      <Gallery />
+
+
     </div>
     
   );
