2cf60944e447b29c7ddbc98765f8d6835ccc1d17
kshitijslays <rajkshitij876@gmail.com>
2025-02-28 10:30:41 +0530

    hj

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import Navbar from "./components/Navbar"
 import HeroSection from "./components/heroSection";
 import WhyAttend from "./components/WhyAttend";
-import Gallery from "./components/Gallery";
-
 function App() {
   return (
     <div className="font-[poppins] bg-white min-h-screen flex flex-col items-center justify-center">
       <Navbar />
       <HeroSection />
       <WhyAttend />
-      <Gallery />
 
 
     </div>
