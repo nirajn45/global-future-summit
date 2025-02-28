133409ae4d27f9f4e04758057cbe01bc6bd27c90
kshitijslays <rajkshitij876@gmail.com>
2025-02-28 10:30:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import HeroSection from "./components/heroSection";
 import WhyAttend from "./components/WhyAttend";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <WhyAttend />
+      <Gallery />
 
 
     </div>
