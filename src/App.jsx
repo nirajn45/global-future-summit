a6091c18a9d1ce8cd0f59981f63f78246efb14ad
Niraj Kumar <guptaniraj915@gmail.com>
2025-03-02 04:12:40 +0530

    Initial commit to my repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,61 @@
-// import React from 'react'
-// import Gfs from "./Pages/gfs"
-// import Ideate from "./Pages/ideate"
-
-// const App = () => {
-//   return (
-//     <>
-//     <Gfs/>
-//     <Ideate/>
-//     </>
-    
-//   )
-// }
-
-// export default App
-
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Outlet, RouterProvider, createBrowserRouter, Form } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import Gfs from "./Pages/gfs";
 import Ideate from "./Pages/ideate";
 import Navbar from "./components1/Navbar";
 import Footer from "./components1/footer";
 import Footer2 from "./components2/Footer2";
 import Navbar2 from "./components2/Navbar2"; 
-import HomePage from "./Pages/gfs";
 import RegistrationForm from "./components2/Form";
-const Layout = ()=>{
-  return(
-    <>
-      <Navbar/>
-      <div>
-        <Outlet/>
-      </div>
-      <Footer/>
-    </>
-  )
-}
 
-const Layout2=()=>{
-  return(
+// Layout for Global Futures Summit (GFS)
+const Layout = () => {
+  return (
     <>
-      <Navbar2/>
+      <Navbar />
       <div>
-        <Outlet/>
+        <Outlet />
       </div>
-      <Footer2/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
+// Layout for Ideate
+const Layout2 = () => {
+  return (
+    <>
+      <Navbar2 />
+      <div>
+        <Outlet />
+      </div>
+      <Footer2 />
+    </>
+  );
+};
+
+// Router Configuration
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Layout/>,
-    children:[
-      {path:"/", element:<HomePage/> },
-      {path:"/register-global-futures-summit", element:<>lol</>}
-    ]
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Gfs /> },
+      { path: "/register-global-futures-summit", element: <>Coming Soon</> }
+    ],
   },
   {
-    path:"/ideate",
-    element: <Layout2/>,
-    children:[
-      {path:"/ideate",element:<Ideate/>},
-      
-    ]
-
+    path: "/ideate",
+    element: <Layout2 />,
+    children: [
+      { path: "", element: <Ideate /> }, // `path: ""` will correctly load Ideate inside `/ideate`
+    ],
   },
-  
-    {path:"/register", element:<RegistrationForm/>}
-  
-])
+  { path: "/register", element: <RegistrationForm /> },
+]);
+
 const App = () => {
-  return (
-    <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
